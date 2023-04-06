9ff72e141b12fe0145856f21308015d3e744426c
DulakshiAmarasooriya <dulakshidilhani8@gmail.com>
2023-04-06 12:22:38 +0530

    Boxing-image Package

---
+++ example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:boxing_images/boxing_images.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Plugin Example'),
+      ),
+      body:BoxingImages(),
+      
+    );
+  }
+}
+
